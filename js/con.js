b4f4b36f4bf58bdf7de97f49d082d88d8090fa0e
SANDIP GHOSH <49060791+ADSANDY18@users.noreply.github.com>
2020-08-24 00:38:24 +0530

    Update con.js

--- js/con.js
+++ js/con.js
@@ -1,6 +1,6 @@
 
 var accessToken = "0482ef822f6fd21b29dc8855aa94faaa5d7eafa5";                      //"1e58b9d46eb4ff5ab8125aa6309ef1796d085f4c" //"e0b8ba1f24ab4c6699e19909835422cf";
-    var baseUrl = "https://api.dialogflow.com/v2/";
+    var baseUrl = "https://api.dialogflow.com/v1/";
     $(document).ready(function() {
       $("#input").keypress(function(event) {
         if (event.which == 13) {
@@ -154,7 +154,7 @@ var accessToken = "0482ef822f6fd21b29dc8855aa94faaa5d7eafa5";                   
            
         type: "POST",
            //20150910
-        url: baseUrl + "query?v=",
+        url: baseUrl + "query?v=20150910",
       //  console.log("++lineAfter_baseUrl");  
         contentType: "application/json; charset=utf-8",
         dataType: "json",
