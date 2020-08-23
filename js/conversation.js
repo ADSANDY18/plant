da85b873678dd6220a29cfc57b1747bfacb2ebe6
SANDIP GHOSH <49060791+ADSANDY18@users.noreply.github.com>
2020-08-24 00:32:00 +0530

    Update conversation.js

--- js/conversation.js
+++ js/conversation.js
@@ -1,6 +1,6 @@
 
 var accessToken = "0482ef822f6fd21b29dc8855aa94faaa5d7eafa5";                      //"1e58b9d46eb4ff5ab8125aa6309ef1796d085f4c" //"e0b8ba1f24ab4c6699e19909835422cf";
-    var baseUrl = "https://api.dialogflow.com/v1/";
+    var baseUrl = "https://api.dialogflow.com/v2/";
     $(document).ready(function() {
       $("#input").keypress(function(event) {
         if (event.which == 13) {
